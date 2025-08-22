433b121f57b56921d85a575581d861571f9c217e
snow520520 <leeseolhang0624@gmail.com>
2025-08-22 17:03:50 +0900

    Initial commit

---
+++ src/main/java/kr/co/iei/SemiProjectCodePawApplication.java
@@ -0,0 +1,13 @@
+package kr.co.iei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiProjectCodePawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemiProjectCodePawApplication.class, args);
+	}
+
+}
